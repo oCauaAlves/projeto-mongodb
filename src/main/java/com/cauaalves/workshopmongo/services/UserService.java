313a1c5a512a7b6c9358251bf99563ecf983897a
Caua Alves <caua.rodriguesalves2004@gmail.com>
2026-02-26 14:14:15 -0300

    User deletion with DELETE

--- src/main/java/com/cauaalves/workshopmongo/services/UserService.java
+++ src/main/java/com/cauaalves/workshopmongo/services/UserService.java
@@ -21,7 +21,6 @@ public class UserService {
         return repo.findAll();
     }
 
-
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
@@ -31,8 +30,12 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
-
 }
